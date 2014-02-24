45ebc65f004ba6dbf87527ed0ca1647313de3dd9
Oliver Santos Piña <oliversantosp@gmail.com>
2014-02-24 16:45:15 -0400

    Added Datetime to each chat message.

--- SuperJugadoresChat/Scripts/signalR.hub.js
+++ SuperJugadoresChat/Scripts/signalR.hub.js
@@ -9,8 +9,15 @@
     var chat = $.connection.chat;
 
     chat.client.addMessage = function (message) {
+        var now = new Date();
+        var strDateTime = [[AddZero(now.getDate()), AddZero(now.getMonth() + 1), now.getFullYear()].join("/"), [AddZero(now.getHours()), AddZero(now.getMinutes())].join(":"), now.getHours() >= 12 ? "PM" : "AM"].join(" ");
+
+        //Pad given value to the left with "0"
+        function AddZero(num) {
+            return (num >= 0 && num < 10) ? "0" + num : num + "";
+        }
         // Add the message to the page. 
-        +$('#discussion').append('<li><strong>User</strong> ' + message + '</li>');
+        +$('#discussion').append('<li><strong>User</strong> ' + message + '<i>' + strDateTime + '</i></li>');
     }
 
     $.connection.hub.start().done(function () {
