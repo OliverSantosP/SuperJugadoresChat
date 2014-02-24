0e6de1cf2831bf4ca2ea42d7872340a2cdb42f7f
Dariel Javier <dariel.javier@theberrycompany.com>
2014-02-24 13:55:00 -0400

    Add SignalR and Jquery to Bundles and remove script from head

--- SuperJugadoresChat/App_Start/BundleConfig.cs
+++ SuperJugadoresChat/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace SuperJugadoresChat
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery.signalR").Include(
+                        "~/Scripts/jquery.signalR-{version}.js",
+                        "~/Scripts/signalR.hub.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
