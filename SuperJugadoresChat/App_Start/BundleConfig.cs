064a691726413cade0f41b17693254cf22ac91bb
Oliver Santos Piña <oliversantosp@gmail.com>
2014-02-24 15:49:31 -0400

    Changed signalR js bundle to specific path.

--- SuperJugadoresChat/App_Start/BundleConfig.cs
+++ SuperJugadoresChat/App_Start/BundleConfig.cs
@@ -15,7 +15,7 @@ namespace SuperJugadoresChat
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery.signalR").Include(
-                        "~/Scripts/jquery.signalR-*",
+                        "~/Scripts/jquery.signalR-2.0.2.js",
                         "~/Scripts/signalR.hub.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
