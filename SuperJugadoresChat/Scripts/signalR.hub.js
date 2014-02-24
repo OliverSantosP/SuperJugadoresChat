301fadc0c57894b922ece4c006caf5fdaa5abab6
Oliver Santos Piña <oliversantosp@gmail.com>
2014-02-24 16:54:19 -0400

    Added validation for empty messages.

--- SuperJugadoresChat/Scripts/signalR.hub.js
+++ SuperJugadoresChat/Scripts/signalR.hub.js
@@ -9,15 +9,18 @@
     var chat = $.connection.chat;
 
     chat.client.addMessage = function (message) {
-        var now = new Date();
-        var strDateTime = [[AddZero(now.getDate()), AddZero(now.getMonth() + 1), now.getFullYear()].join("/"), [AddZero(now.getHours()), AddZero(now.getMinutes())].join(":"), now.getHours() >= 12 ? "PM" : "AM"].join(" ");
+        if (message!='') {
+            var now = new Date();
+            var strDateTime = [[AddZero(now.getDate()), AddZero(now.getMonth() + 1), now.getFullYear()].join("/"), [AddZero(now.getHours()), AddZero(now.getMinutes())].join(":"), now.getHours() >= 12 ? "PM" : "AM"].join(" ");
 
-        //Pad given value to the left with "0"
-        function AddZero(num) {
-            return (num >= 0 && num < 10) ? "0" + num : num + "";
+            //Pad given value to the left with "0"
+            function AddZero(num) {
+                return (num >= 0 && num < 10) ? "0" + num : num + "";
+            }
+            // Add the message to the page. 
+            +$('#discussion').append('<li><strong>User</strong> ' + message + '<i> ' + strDateTime + '</i></li>');
         }
-        // Add the message to the page. 
-        +$('#discussion').append('<li><strong>User</strong> ' + message + '<i>' + strDateTime + '</i></li>');
+        
     }
 
     $.connection.hub.start().done(function () {
